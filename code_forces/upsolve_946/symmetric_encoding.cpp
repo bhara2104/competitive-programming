945a1fd0d428fdc5e3f8c5f99f8460ef54023476
bharath <bharathbk2121@gmail.com>
2024-05-22 08:07:46 +0530

    upsolving code_forces 946

--- code_forces/upsolve_946/symmetric_encoding.cpp
+++ code_forces/upsolve_946/symmetric_encoding.cpp
@@ -10,20 +10,24 @@ int main(){
 		string a ; 
 		cin >> a ; 
 		set<char> c ; 
-		for(auto& it : c){
-			c.insert(it) ;
-		}
 
-		vector<char> vec(c.begin() , c.end()) ; 
-		map<char,char> mp ;
-		for(int i = 0 ; i < len ; i++){
-			mp[vec[i]] = mp[vec[len - i - 1]] ; 
+		vector<char> vec ; 
+		for(int i = 0 ; i<a.size() ; i++){
+			if(c.count(a[i])==0){
+				c.insert(a[i]) ;
+				vec.push_back(a[i]);
+				// cout << a[i] ;
+			}
 		}
+		sort(vec.begin() , vec.end()) ; 
+		map<char,char> mp ;	
 
-		for(auto it : a){
-			cout<< mp[it];
-		}
-		cout<< endl ; 
+		for(int i = 0 ; i < vec.size() ; i++){
+			// cout << vec[i] << vec[vec.size() - i - 1] ;
+			mp[vec[i]] = vec[vec.size() - i - 1] ; 
+		} 
+		 for(int i = 0 ; i<len ; i++) cout << mp[a[i]];
+		cout<<"\n" ; 
 
 
 	}
